42b9eb8b114133827316497bcbb72c52270d3384
Samuel Moritz <samuel.moritz@gmail.com>
2015-10-01 20:49:36 +0200

    Add trubbl-monolith

---
+++ trubbl-monolith/src/main/java/trubbl/issues/rest/IssueController.java
@@ -0,0 +1,30 @@
+package trubbl.issues.rest;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import trubbl.issues.domain.Issue;
+import trubbl.issues.domain.IssueRepository;
+
+import javax.inject.Inject;
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/issues")
+public class IssueController {
+
+    @Inject
+    private IssueRepository repository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Collection<Issue> searchIssues() {
+        return repository.search();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void createIssue(@RequestBody Issue issue) {
+        repository.create(issue);
+    }
+
+}
